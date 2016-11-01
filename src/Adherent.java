b766200c8bedd36858de459b1ee570d7cad3b039
cc <cc@dy.local>
2016-11-01 15:52:16 -0400

    "asdasd"

--- src/Adherent.java
+++ src/Adherent.java
@@ -2,8 +2,7 @@
 public class Adherent extends Personne {
 	private int intNumeroInscription;
 
-	public Adherent(int intNumeroTelephone, String strNom, String strPrenom, String strAdresse,
-			int intNumeroInscription) {
+	public Adherent(int intNumeroTelephone, String strNom, String strPrenom, String strAdresse,int intNumeroInscription) {
 		super(strNom, strPrenom, strAdresse, intNumeroTelephone);
 		this.intNumeroInscription = intNumeroInscription;
 	}
