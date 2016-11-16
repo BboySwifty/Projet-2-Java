7cb03cce4e8f07efe268c7761240960cfec7d15a
A.Abboud <A.Abboud@790-444-02.interne.college-gerald-godin.qc.ca>
2016-11-16 16:23:39 -0500

    asd

--- src/données/TableModelCollection.java
+++ src/données/TableModelCollection.java
@@ -10,7 +10,7 @@ public class TableModelCollection extends AbstractTableModel {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -4492531178394267055L;
 	@SuppressWarnings("rawtypes")
 	
 	ArrayList<ArrayList> alDonnees = new ArrayList<ArrayList>();
