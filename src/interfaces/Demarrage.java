342e05bf890d4c5d73c75dd9c31a30b6c41ef787
a.chen <a.chen@790-444-03.interne.college-gerald-godin.qc.ca>
2016-11-16 15:12:58 -0500

    123

--- src/interfaces/Demarrage.java
+++ src/interfaces/Demarrage.java
@@ -4,7 +4,6 @@ public class Demarrage
 {
 	public static void main (String[]args)
 	{
-		Interface face = new Interface("str");
 		Connexion connexion = new Connexion();
 		connexion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
